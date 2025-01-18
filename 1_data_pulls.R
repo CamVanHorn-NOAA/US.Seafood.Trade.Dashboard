1b550e025acfab9d3412e519ab77d154cb97d33a
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-17 16:16:03 -0800

    create script 1 for data pulls

---
+++ 1_data_pulls.R
@@ -0,0 +1,9 @@
+# Script 1: Pull Data for Seafood Dashboard
+# Data sources include the fisheries one-stop shop (FOSS, 
+  # https://www.fisheries.noaa.gov/foss/f?p=215:2:742090237412:::::) and SQL
+  # databases. 
+# Contact: Cameron Van Horn
+#          cameron.vanhorn@noaa.gov
+
+
+
