558d5eabfd6375ec84cea045c7ed2373225f9409
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-17 16:16:43 -0800

    assign google drive

--- 1_data_pulls.R
+++ 1_data_pulls.R
@@ -11,3 +11,13 @@
 if(!require("googledrive")) install.packages("googledrive")
 if(!require("dplyr"))   install.packages("dplyr")
 
+####################################
+### CONNECT TO YOUR GOOGLE DRIVE ###
+####################################
+# The googledrive package comes from tidyverse and enables R users to connect
+  # directly to google drive for access to files.
+# We can run drive_find(n_max = XX) to obtain access credentials and link
+  # the R session to a google drive account
+drive_find(n_max = 25)
+# the output is simply named files in your google drive
+
