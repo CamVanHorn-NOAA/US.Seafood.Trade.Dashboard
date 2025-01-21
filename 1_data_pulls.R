14ccbf8b530d719ccb56c460e783fdf4cadb6495
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-21 15:30:44 -0800

    remove (, ) from column names
    best data practice. can make efficient later

--- 1_data_pulls.R
+++ 1_data_pulls.R
@@ -48,12 +48,16 @@ foss_exports_1524 <- read.csv('foss_exports_15-24.csv') %>%
   # use setNames from 'stats' to assign first row values as column names
   setNames(.[1, ]) %>%
   rename_with( ~ toupper(gsub(' ', '_', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub('(', '', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub(')', '', .x, fixed = T))) %>%
   # remove first row
   .[-1, ]
 
 foss_exports_0414 <- read.csv('foss_exports_04-14.csv') %>%
   setNames(.[1, ]) %>%
   rename_with( ~ toupper(gsub(' ', '_', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub('(', '', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub(')', '', .x, fixed = T))) %>%
   .[-1, ]
 
 # combine data (stack)
@@ -64,11 +68,15 @@ foss_exports <- bind_rows(foss_exports_0414, foss_exports_1524)
 foss_imports_1524 <- read.csv('foss_imports_15-24.csv') %>%
   setNames(.[1, ]) %>%
   rename_with( ~ toupper(gsub(' ', '_', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub('(', '', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub(')', '', .x, fixed = T))) %>%
   .[-1, ]
 
 foss_imports_0414 <- read.csv('foss_imports_04-14.csv') %>%
   setNames(.[1, ]) %>%
   rename_with( ~ toupper(gsub(' ', '_', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub('(', '', .x, fixed = T))) %>%
+  rename_with( ~ toupper(gsub(')', '', .x, fixed = T))) %>%
   .[-1, ]
 
 # combine data (stack)
