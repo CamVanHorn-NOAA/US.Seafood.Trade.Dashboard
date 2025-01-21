73d5f7df47434ffe1908590c45759ca054fec530
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-21 14:07:02 -0800

    grab most recent data file from drive

--- 2_data_munge.R
+++ 2_data_munge.R
@@ -13,3 +13,20 @@
 if(!require("googledrive")) install.packages("googledrive")
 if(!require("tidyverse")) install.packages("tidyverse")
 
+# Authorize link to google drive
+drive_auth()
+
+# Grab most recent data file
+  # only take first two columns (the third column is a list of metadata)
+data_file <- drive_find(pattern = 'seafood_trade_data_pull')[, 1:2] %>%
+  # set column names to caps for best data practice
+  rename_with(toupper) %>%
+  # isolate date of data creation into separate string
+    # recall that script 1 embeds date of data creation into file name
+  mutate(DATE = str_remove(NAME, 'seafood_trade_data_pull_'),
+         DATE = str_remove(DATE, '.RData'),
+         # format date to find the most recent date
+         date = as.Date(DATE, format = '%m_%d_%y')) %>%
+  # filter for most recent date (max)
+  filter(DATE == max(DATE)) %>%
+  select(NAME)
