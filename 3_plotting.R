6ded356ebec2f71728cfb98c9671402e9e5ae794
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-23 11:19:08 -0800

    set up script 3 for plotting

---
+++ 3_plotting.R
@@ -0,0 +1,48 @@
+# Script 3: Visualize Seafood Trade Data
+# See 2_data_munge for data curation and 1_data_pulls for data sourcing
+
+# Contact: Cameron Van Horn
+#          cameron.vanhorn@noaa.gov
+
+#########################
+### PACKAGES AND DATA ###
+#########################
+# Packages ---------------------------------------------------------------------
+if(!require("googledrive")) install.packages("googledrive")
+if(!require("tidyverse")) install.packages("tidyverse")
+
+# Pull Data --------------------------------------------------------------------
+# Authorize link to google drive
+drive_auth()
+
+# Grab most recent data file
+# only take first two columns (the third column is a list of metadata)
+data_file <- drive_find(pattern = 'seafood_trade_data_munge')[, 1:2] %>%
+  # set column names to caps for best data practice
+  rename_with(toupper) %>%
+  # isolate date of data creation into separate string
+  # recall that script 1 embeds date of data creation into file name
+  mutate(DATE = str_remove(NAME, 'seafood_trade_data_pull_'),
+         DATE = str_remove(DATE, '.RData'),
+         # format date to find the most recent date
+         date = as.Date(DATE, format = '%m_%d_%y')) %>%
+  # filter for most recent date (max)
+  filter(DATE == max(DATE)) %>%
+  select(NAME)
+
+# Download the data
+drive_download(data_file$NAME,
+               overwrite = T)
+
+# Load the data
+load(data_file$NAME)
+# clean environment
+rm(data_file)
+
+####################
+### SIMPLE PLOTS ###
+####################
+# Exports ----------------------------------------------------------------------
+# Imports ----------------------------------------------------------------------
+# Trade Balances ---------------------------------------------------------------
+# Processed Products ----------------------------------------------------------- 
