a3b5d2dc7b43e3feba71e37309924747a22a92a3
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-01-28 16:17:10 -0800

    attach species_ref data to trade_data

--- 2_data_munge.R
+++ 2_data_munge.R
@@ -56,20 +56,24 @@ foss_exports <- foss_exports %>%
          CENSUS_DISTRICT_CODE = as.numeric(CENSUS_DISTRICT_CODE),
          FAO_COUNTRY_CODE = as.numeric(FAO_COUNTRY_CODE),
          YEAR = as.numeric(YEAR)) %>%
+  # use species_ref to attach species info to products
+  left_join(species_ref %>% 
+              select(HTS_NUMBER, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
+              # remove duplicates to not create many-to-many relationships for
+                # the join
+              distinct()) %>%
   # arrange by year then country name 
-  arrange(YEAR, COUNTRY_NAME)
-
-# Calculate standard prices with 2023 index
-# We standardize prices by setting a year as a reference for inflation indexing
-  # Effectively, by setting 2023 as our reference year, we can calculate what
-  # prices from prior years would be in 2023 dollars
-  # This is accounts for price fluctuations exclusively due to inflation
-# To standardize, we set the reference year's index (2023) as the numerator 
-  # and a given year's index (e.g., 2020) as the denominator
-  # Then multiply this value by the price of the good in the given year to 
-  # determine it's value in real 2023 dollars
-  # We calculated the Index value in script 1
-foss_exports <- foss_exports %>%
+  arrange(YEAR, COUNTRY_NAME) %>%
+  # Calculate standard prices with 2023 index
+  # We standardize prices by setting a year as a reference for inflation indexing
+    # Effectively, by setting 2023 as our reference year, we can calculate what
+    # prices from prior years would be in 2023 dollars
+    # This is accounts for price fluctuations exclusively due to inflation
+  # To standardize, we set the reference year's index (2023) as the numerator 
+    # and a given year's index (e.g., 2020) as the denominator
+    # Then multiply this value by the price of the good in the given year to 
+    # determine it's value in real 2023 dollars
+    # We calculated the Index value in script 1
   left_join(def_index %>% select(YEAR, INDEX)) %>%
   mutate(EXP_VALUE_2023USD = VALUE_USD * INDEX) %>%
   select(-INDEX)
@@ -81,9 +85,9 @@ foss_exports <- foss_exports %>%
   # country name (exported to), customs district (exported from)
 exports_products_smry <- foss_exports %>%
   select(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, FAO_COUNTRY_CODE,
-         PRODUCT_NAME) %>%
+         PRODUCT_NAME, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   group_by(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, 
-           FAO_COUNTRY_CODE) %>%
+           FAO_COUNTRY_CODE, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   summarise(EXP_PRODUCT_DIVERSITY = n_distinct(PRODUCT_NAME),
             .groups = 'drop')
 
@@ -91,9 +95,10 @@ exports_products_smry <- foss_exports %>%
   # country name (exported to), customs district (exported from)
 exports_price_smry <- foss_exports %>%
   select(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, FAO_COUNTRY_CODE,
-         VALUE_USD, EXP_VALUE_2023USD, VOLUME_KG) %>%
+         VALUE_USD, EXP_VALUE_2023USD, VOLUME_KG, GROUP_NAME, GROUP_TS, 
+         GROUP_CBP) %>%
   group_by(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, 
-           FAO_COUNTRY_CODE) %>%
+           FAO_COUNTRY_CODE, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   summarise(across(where(is.numeric), sum),
             .groups = 'drop') %>%
   mutate(EXP_AVERAGE_PRICE_PER_KG = VALUE_USD / VOLUME_KG,
@@ -113,9 +118,10 @@ foss_imports <- foss_imports %>%
          FAO_COUNTRY_CODE = as.numeric(FAO_COUNTRY_CODE),
          CALCULATED_DUTY_USD = as.numeric(gsub(',', '', CALCULATED_DUTY_USD)),
          YEAR = as.numeric(YEAR)) %>%
-  arrange(YEAR, COUNTRY_NAME)
-
-foss_imports <- foss_imports %>%
+  left_join(species_ref %>% 
+              select(HTS_NUMBER, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
+              distinct()) %>%
+  arrange(YEAR, COUNTRY_NAME) %>%
   left_join(def_index %>% select(YEAR, INDEX)) %>%
   mutate(IMP_VALUE_2023USD = VALUE_USD * INDEX,
          IMP_CALCULATED_DUTY_2023USD = CALCULATED_DUTY_USD * INDEX) %>%
@@ -125,18 +131,18 @@ foss_imports <- foss_imports %>%
 # Data summarizing
 imports_products_smry <- foss_imports %>%
   select(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, FAO_COUNTRY_CODE,
-         PRODUCT_NAME) %>%
+         PRODUCT_NAME, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   group_by(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT,
-           FAO_COUNTRY_CODE) %>%
+           FAO_COUNTRY_CODE, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   summarise(IMP_PRODUCT_DIVERSITY = n_distinct(PRODUCT_NAME),
             .groups = 'drop')
 
 imports_price_smry <- foss_imports %>%
   select(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, FAO_COUNTRY_CODE,
          VALUE_USD, VOLUME_KG, IMP_VALUE_2023USD, CALCULATED_DUTY_USD,
-         IMP_CALCULATED_DUTY_2023USD) %>%
+         IMP_CALCULATED_DUTY_2023USD, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   group_by(YEAR, CONTINENT, COUNTRY_NAME, US_CUSTOMS_DISTRICT, 
-           FAO_COUNTRY_CODE) %>%
+           FAO_COUNTRY_CODE, GROUP_NAME, GROUP_TS, GROUP_CBP) %>%
   summarise(across(where(is.numeric), sum),
             .groups = 'drop') %>%
   mutate(IMP_AVERAGE_PRICE_PER_KG = VALUE_USD / VOLUME_KG,
@@ -163,10 +169,10 @@ imports_smry <- imports_smry %>%
 
 # full_join the tables to account for countries or customs districts that 
   # exclusively import or export
-trade_data <- full_join(exports_smry, imports_smry)
+trade_data <- full_join(exports_smry, imports_smry) 
 # The resulting data frame includes import and export data attached to each
-  # US Custom's District and Country of Origin or Export for every year from 
-  # 2004 - 2024
+  # US Custom's District and Country of Origin or Export, with species data, 
+  # for every year from 2004 - 2024
 
 
 # Processed Products -----------------------------------------------------------
