93ae68dfb4e34c74adc41440845df49df400333b
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-07-23 11:58:19 -0700

    coerce price y-axis to use same scalefactor

--- dashboard_draft/app.R
+++ dashboard_draft/app.R
@@ -1003,15 +1003,10 @@ plot_trade <- function(data, plot_format, export = F, import = F, species) {
     # for the two axes to work in ggplot, we need a scaling factor to apply
     # The scaling factor will coerce the price values to work against the 
     # main y axis of value while retaining price trends across years
-    max_value <- data %>%
-      slice_max(!!y, n = 1) %>%
-      select(!!y) %>%
-      pull()
-    
-    max_price <- data %>%
-      slice_max(!!y2, n = 1) %>%
-      select(!!y2) %>%
-      pull()
+    # To ensure the plots of exports and imports are coerced to the same axis,
+    # use the same scale factor for all plots by using the max value and 
+    # max price, which may not necessarily be both exports or imports
+    scale_factor <- y_max / y2_max
     
     scale_factor <- max_value / max_price
     plot <- 
