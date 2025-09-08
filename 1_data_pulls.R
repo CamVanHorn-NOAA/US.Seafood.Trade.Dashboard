8a868acce5cbca4973a348fea7deac2675856261
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2025-09-08 16:39:44 -0700

    update save file

--- 1_data_pulls.R
+++ 1_data_pulls.R
@@ -241,8 +241,8 @@ file_name <- paste0('seafood_trade_data_pull_',
 
 # create the file
   # NOTE: add new data to this list upon creation in this script
-save(list = c('foss_exports', 'foss_imports', 'foss_pp', 'def_index',
-              'species_ref', 'foss_com_landings', 'pp_landings_map',
+save(list = c('foss_exports', 'foss_imports', 'pp_processed', 'def_index',
+              'species_ref', 'foss_com_landings', 
               'trade_map', 'landings_map', 'pp_map'),
      file = file_name)
 
