4ab0ba1cb7981003055c73fe41b1b44f11009118
Cameron Van Horn <cameron.vanhorn@noaa.gov>
2026-02-27 12:46:40 -0800

    simplify call for date grab

--- 1_data_pulls.R
+++ 1_data_pulls.R
@@ -280,13 +280,13 @@ conversion_factors <- read.csv('conversion_factors/conversion_factors.csv') %>%
 
 # Metadata (time of data access) -----------------------------------------------
 trade_date <- 
-  format(file.info('C:/Users/cameron.vanhorn/Documents/GitHub/US.Seafood.Trade.Dashboard/foss_exports_15-25.csv')$ctime,
+  format(file.info('foss_exports_15-25.csv')$ctime,
          '%m/%d/%Y')
 landings_date <- 
-  format(file.info('C:/Users/cameron.vanhorn/Documents/GitHub/US.Seafood.Trade.Dashboard/foss_com_landings_15-24.csv')$ctime,
+  format(file.info('foss_com_landings_15-24.csv')$ctime,
          '%m/%d/%Y')
 products_date <- 
-  format(file.info('C:/Users/cameron.vanhorn/Documents/GitHub/US.Seafood.Trade.Dashboard/pp_processed.csv')$ctime,
+  format(file.info('pp_processed.csv')$ctime,
          '%m/%d/%Y')
 
 #####################
